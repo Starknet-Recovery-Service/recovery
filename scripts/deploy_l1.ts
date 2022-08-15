d0ca40c0bf9e306fa4e05c53ef6b3956bc2ea014
yeungparkhay <yeungparkhay@gmail.com>
2022-08-15 19:14:41 +0100

    redeployed contracts

--- scripts/deploy_l1.ts
+++ scripts/deploy_l1.ts
@@ -1,7 +1,7 @@
 // To deploy: npx hardhat run --network goerli scripts/deploy_l1.ts
-// To verify: npx hardhat verify --network goerli 0xC6362C14E0F09DD077Be0683B24cb225845f35CA "0xde29d060D45901Fb19ED6C6e959EB22d8626708e"
+// To verify: npx hardhat verify --network goerli 0x4b4fE3787407A999f41703e340F8D33B143443d7 "0xde29d060D45901Fb19ED6C6e959EB22d8626708e"
 
-// Deployment address: 0xC6362C14E0F09DD077Be0683B24cb225845f35CA
+// Deployment address: 0x4b4fE3787407A999f41703e340F8D33B143443d7
 
 import { ethers } from "hardhat"
 
@@ -14,7 +14,7 @@ async function main() {
     const gatewayContract = await ethers.getContractFactory("GatewayContract")
     const gateway = await gatewayContract.deploy(starknetCoreContractAddress)
 
-    console.log("TestGatewayContract address:", gateway.address)
+    console.log("GatewayContract address:", gateway.address)
 }
 
 main()
--- scripts/deploy_l2.ts
+++ scripts/deploy_l2.ts
@@ -1,14 +1,14 @@
 // To deploy: npx hardhat run --network alpha scripts/deploy_l2.ts
-// To verify: npx hardhat starknet-verify --starknet-network alpha --path contracts/starknet/StorageProver.cairo --compiler-version 0.9.1 --address 0x0485b6898e9399da5bc8ee964ff1d071c0b1dace8fca8ce22731ba225d07d39f
+// To verify: npx hardhat starknet-verify --starknet-network alpha --path contracts/starknet/StorageProver.cairo --compiler-version 0.9.1 --address 0x01842fdb2938386ee745ea5d77cea68736d46665f510fea7ed45cd8110e39937
 
-// Deployment address: 0x0485b6898e9399da5bc8ee964ff1d071c0b1dace8fca8ce22731ba225d07d39f
+// Deployment address: 0x01842fdb2938386ee745ea5d77cea68736d46665f510fea7ed45cd8110e39937
 
 import { starknet } from "hardhat"
 
 async function main() {
     const factRegistryAddressFelt = 945446405930356733034975194720402002914171111673876520981451176768939208501n
     const L1HeadersStoreAddress = 837485042664063856828332244883203579059038878551051469326645034621781373797n
-    const L1GatewayAddress = 1131588252909065638474120670588369352712811132362n
+    const L1GatewayAddress = 429955882159492657148005544722965770209018004439n
 
     const storageProverFactory = await starknet.getContractFactory("StorageProver")
     const storageProver = await storageProverFactory.deploy({
