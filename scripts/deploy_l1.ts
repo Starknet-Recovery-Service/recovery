a38c6c9f12d4cb5102a90e8763961d76101860ed
yeungparkhay <yeungparkhay@gmail.com>
2022-08-15 14:32:54 +0100

    compiled and deployed contracts, updated addresses

--- scripts/deploy_l1.ts
+++ scripts/deploy_l1.ts
@@ -1,7 +1,7 @@
 // To deploy: npx hardhat run --network goerli scripts/deploy_l1.ts
-// To verify: npx hardhat verify --network goerli <address> "0xde29d060D45901Fb19ED6C6e959EB22d8626708e"
+// To verify: npx hardhat verify --network goerli 0xC6362C14E0F09DD077Be0683B24cb225845f35CA "0xde29d060D45901Fb19ED6C6e959EB22d8626708e"
 
-// Deployment address: 0xDC20B3816E7ca8e6978325e980B4911F8A097883
+// Deployment address: 0xC6362C14E0F09DD077Be0683B24cb225845f35CA
 
 import { ethers } from "hardhat"
 
--- scripts/deploy_l2.ts
+++ scripts/deploy_l2.ts
@@ -1,14 +1,14 @@
-// To deploy: npx hardhat run --network alpha-goerli scripts/deploy_l2.ts
-// To verify: npx hardhat starknet-verify --starknet-network alpha-goerli --path contracts/starknet/StorageProver.cairo --compiler-version 0.9.1 --address 0x0358fa86a803e16901367550765ecbfee5c7e8c3e5efd6043e42aab947ddccba
+// To deploy: npx hardhat run --network alpha scripts/deploy_l2.ts
+// To verify: npx hardhat starknet-verify --starknet-network alpha --path contracts/starknet/StorageProver.cairo --compiler-version 0.9.1 --address 0x0485b6898e9399da5bc8ee964ff1d071c0b1dace8fca8ce22731ba225d07d39f
 
-// Deployment address: 0x0358fa86a803e16901367550765ecbfee5c7e8c3e5efd6043e42aab947ddccba
+// Deployment address: 0x0485b6898e9399da5bc8ee964ff1d071c0b1dace8fca8ce22731ba225d07d39f
 
 import { starknet } from "hardhat"
 
 async function main() {
     const factRegistryAddressFelt = 945446405930356733034975194720402002914171111673876520981451176768939208501n
     const L1HeadersStoreAddress = 837485042664063856828332244883203579059038878551051469326645034621781373797n
-    const L1GatewayAddress = 1256707230570067143218012969760719261706450729091n
+    const L1GatewayAddress = 1131588252909065638474120670588369352712811132362n
 
     const storageProverFactory = await starknet.getContractFactory("StorageProver")
     const storageProver = await storageProverFactory.deploy({
